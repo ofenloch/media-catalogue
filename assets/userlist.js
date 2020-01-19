1112fe2ab617ad0bdc4c1f75f02184c97476f603
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-19 11:49:10 +0100

    add some examples from the tutorial

---
+++ assets/userlist.js
@@ -0,0 +1,505 @@
+
+USERS = [
+  {
+    id: 10001,
+    emp_no: 10001,
+    gender: 'M',
+    hire_date: '1986-06-26',
+    last_name: 'Facello',
+    birth_date: '1953-09-02',
+    first_name: 'Georgi',
+    middle_names: 'John F.'
+  },
+  {
+    id: 10002,
+    emp_no: 10002,
+    gender: 'F',
+    hire_date: '1985-11-21',
+    last_name: 'Simmel',
+    birth_date: '1964-06-02',
+    first_name: 'Bezalel',
+    middle_names: 'H. Henry'
+  },
+  {
+    id: 10003,
+    emp_no: 10003,
+    gender: 'M',
+    hire_date: '1986-08-28',
+    last_name: 'Bamford',
+    birth_date: '1959-12-03',
+    first_name: 'Parto',
+    middle_names: ''
+  },
+  {
+    id: 10004,
+    emp_no: 10004,
+    gender: 'M',
+    hire_date: '1986-12-01',
+    last_name: 'Koblick',
+    birth_date: '1954-05-01',
+    first_name: 'Chirstian',
+    middle_names: 'Richard'
+  },
+  {
+    id: 10005,
+    emp_no: 10005,
+    gender: 'M',
+    hire_date: '1989-09-12',
+    last_name: 'Maliniak',
+    birth_date: '1955-01-21',
+    first_name: 'Kyoichi',
+    middle_names: 'Georgy Sandor'
+  },
+  {
+    id: 10006,
+    emp_no: 10006,
+    gender: 'F',
+    hire_date: '1989-06-02',
+    last_name: 'Preusig',
+    birth_date: '1953-04-20',
+    first_name: 'Anneke',
+    middle_names: 'S. Jane'
+  },
+  {
+    id: 10007,
+    emp_no: 10007,
+    gender: 'F',
+    hire_date: '1989-02-10',
+    last_name: 'Zielinski',
+    birth_date: '1957-05-23',
+    first_name: 'Tzvetan',
+    middle_names: ''
+  },
+  {
+    id: 10008,
+    emp_no: 10008,
+    gender: 'M',
+    hire_date: '1994-09-15',
+    last_name: 'Kalloufi',
+    birth_date: '1958-02-19',
+    first_name: 'Saniya',
+    middle_names: ''
+  },
+  {
+    id: 10009,
+    emp_no: 10009,
+    gender: 'F',
+    hire_date: '1985-02-18',
+    last_name: 'Peac',
+    birth_date: '1952-04-19',
+    first_name: 'Sumant',
+    middle_names: 'Charlotte'
+  },
+  {
+    id: 10010,
+    emp_no: 10010,
+    gender: 'F',
+    hire_date: '1989-08-24',
+    last_name: 'Piveteau',
+    birth_date: '1963-06-01',
+    first_name: 'Duangkaew',
+    middle_names: 'Harriette W.'
+  },
+  {
+    id: 10011,
+    emp_no: 10011,
+    gender: 'F',
+    hire_date: '1990-01-22',
+    last_name: 'Sluis',
+    birth_date: '1953-11-07',
+    first_name: 'Mary',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10012,
+    emp_no: 10012,
+    gender: 'M',
+    hire_date: '1992-12-18',
+    last_name: 'Bridgland',
+    birth_date: '1960-10-04',
+    first_name: 'Patricio',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10013,
+    emp_no: 10013,
+    gender: 'M',
+    hire_date: '1985-10-20',
+    last_name: 'Terkki',
+    birth_date: '1963-06-07',
+    first_name: 'Eberhardt',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10014,
+    emp_no: 10014,
+    gender: 'M',
+    hire_date: '1987-03-11',
+    last_name: 'Genin',
+    birth_date: '1956-02-12',
+    first_name: 'Berni',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10015,
+    emp_no: 10015,
+    gender: 'M',
+    hire_date: '1987-07-02',
+    last_name: 'Nooteboom',
+    birth_date: '1959-08-19',
+    first_name: 'Guoxiang',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10016,
+    emp_no: 10016,
+    gender: 'M',
+    hire_date: '1995-01-27',
+    last_name: 'Cappelletti',
+    birth_date: '1961-05-02',
+    first_name: 'Kazuhito',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10017,
+    emp_no: 10017,
+    gender: 'F',
+    hire_date: '1993-08-03',
+    last_name: 'Bouloucos',
+    birth_date: '1958-07-06',
+    first_name: 'Cristinel',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10018,
+    emp_no: 10018,
+    gender: 'F',
+    hire_date: '1987-04-03',
+    last_name: 'Peha',
+    birth_date: '1954-06-19',
+    first_name: 'Kazuhide',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10019,
+    emp_no: 10019,
+    gender: 'M',
+    hire_date: '1999-04-30',
+    last_name: 'Haddadi',
+    birth_date: '1953-01-23',
+    first_name: 'Lillian',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10020,
+    emp_no: 10020,
+    gender: 'M',
+    hire_date: '1991-01-26',
+    last_name: 'Warwick',
+    birth_date: '1952-12-24',
+    first_name: 'Mayuko',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10021,
+    emp_no: 10021,
+    gender: 'M',
+    hire_date: '1988-02-10',
+    last_name: 'Erde',
+    birth_date: '1960-02-20',
+    first_name: 'Ramzi',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10022,
+    emp_no: 10022,
+    gender: 'M',
+    hire_date: '1995-08-22',
+    last_name: 'Famili',
+    birth_date: '1952-07-08',
+    first_name: 'Shahaf',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10023,
+    emp_no: 10023,
+    gender: 'F',
+    hire_date: '1989-12-17',
+    last_name: 'Montemayor',
+    birth_date: '1953-09-29',
+    first_name: 'Bojan',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10024,
+    emp_no: 10024,
+    gender: 'F',
+    hire_date: '1997-05-19',
+    last_name: 'Pettey',
+    birth_date: '1958-09-05',
+    first_name: 'Suzette',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10025,
+    emp_no: 10025,
+    gender: 'M',
+    hire_date: '1987-08-17',
+    last_name: 'Heyers',
+    birth_date: '1958-10-31',
+    first_name: 'Prasadram',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10026,
+    emp_no: 10026,
+    gender: 'M',
+    hire_date: '1995-03-20',
+    last_name: 'Berztiss',
+    birth_date: '1953-04-03',
+    first_name: 'Yongqiao',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10027,
+    emp_no: 10027,
+    gender: 'F',
+    hire_date: '1989-07-07',
+    last_name: 'Reistad',
+    birth_date: '1962-07-10',
+    first_name: 'Divier',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10028,
+    emp_no: 10028,
+    gender: 'M',
+    hire_date: '1991-10-22',
+    last_name: 'Tempesti',
+    birth_date: '1963-11-26',
+    first_name: 'Domenick',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10029,
+    emp_no: 10029,
+    gender: 'M',
+    hire_date: '1985-11-20',
+    last_name: 'Herbst',
+    birth_date: '1956-12-13',
+    first_name: 'Otmar',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10030,
+    emp_no: 10030,
+    gender: 'M',
+    hire_date: '1994-02-17',
+    last_name: 'Demeyer',
+    birth_date: '1958-07-14',
+    first_name: 'Elvis',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10031,
+    emp_no: 10031,
+    gender: 'M',
+    hire_date: '1991-09-01',
+    last_name: 'Joslin',
+    birth_date: '1959-01-27',
+    first_name: 'Karsten',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10032,
+    emp_no: 10032,
+    gender: 'F',
+    hire_date: '1990-06-20',
+    last_name: 'Reistad',
+    birth_date: '1960-08-09',
+    first_name: 'Jeong',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10033,
+    emp_no: 10033,
+    gender: 'M',
+    hire_date: '1987-03-18',
+    last_name: 'Merlo',
+    birth_date: '1956-11-14',
+    first_name: 'Arif',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10034,
+    emp_no: 10034,
+    gender: 'M',
+    hire_date: '1988-09-21',
+    last_name: 'Swan',
+    birth_date: '1962-12-29',
+    first_name: 'Bader',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10035,
+    emp_no: 10035,
+    gender: 'M',
+    hire_date: '1988-09-05',
+    last_name: 'Chappelet',
+    birth_date: '1953-02-08',
+    first_name: 'Alain',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10036,
+    emp_no: 10036,
+    gender: 'M',
+    hire_date: '1992-01-03',
+    last_name: 'Portugali',
+    birth_date: '1959-08-10',
+    first_name: 'Adamantios',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10037,
+    emp_no: 10037,
+    gender: 'M',
+    hire_date: '1990-12-05',
+    last_name: 'Makrucki',
+    birth_date: '1963-07-22',
+    first_name: 'Pradeep',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10038,
+    emp_no: 10038,
+    gender: 'M',
+    hire_date: '1989-09-20',
+    last_name: 'Lortz',
+    birth_date: '1960-07-20',
+    first_name: 'Huan',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10039,
+    emp_no: 10039,
+    gender: 'M',
+    hire_date: '1988-01-19',
+    last_name: 'Brender',
+    birth_date: '1959-10-01',
+    first_name: 'Alejandro',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10040,
+    emp_no: 10040,
+    gender: 'F',
+    hire_date: '1993-02-14',
+    last_name: 'Meriste',
+    birth_date: '1959-09-13',
+    first_name: 'Weiyi',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10041,
+    emp_no: 10041,
+    gender: 'F',
+    hire_date: '1989-11-12',
+    last_name: 'Lenart',
+    birth_date: '1959-08-27',
+    first_name: 'Uri',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10042,
+    emp_no: 10042,
+    gender: 'F',
+    hire_date: '1993-03-21',
+    last_name: 'Stamatiou',
+    birth_date: '1956-02-26',
+    first_name: 'Magy',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10043,
+    emp_no: 10043,
+    gender: 'M',
+    hire_date: '1990-10-20',
+    last_name: 'Tzvieli',
+    birth_date: '1960-09-19',
+    first_name: 'Yishay',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10044,
+    emp_no: 10044,
+    gender: 'F',
+    hire_date: '1994-05-21',
+    last_name: 'Casley',
+    birth_date: '1961-09-21',
+    first_name: 'Mingsen',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10045,
+    emp_no: 10045,
+    gender: 'M',
+    hire_date: '1989-09-02',
+    last_name: 'Shanbhogue',
+    birth_date: '1957-08-14',
+    first_name: 'Moss',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10046,
+    emp_no: 10046,
+    gender: 'M',
+    hire_date: '1992-06-20',
+    last_name: 'Rosenbaum',
+    birth_date: '1960-07-23',
+    first_name: 'Lucien',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10047,
+    emp_no: 10047,
+    gender: 'M',
+    hire_date: '1989-03-31',
+    last_name: 'Nyanchama',
+    birth_date: '1952-06-29',
+    first_name: 'Zvonko',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10048,
+    emp_no: 10048,
+    gender: 'M',
+    hire_date: '1985-02-24',
+    last_name: 'Syrotiuk',
+    birth_date: '1963-07-11',
+    first_name: 'Florian',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10049,
+    emp_no: 10049,
+    gender: 'F',
+    hire_date: '1992-05-04',
+    last_name: 'Tramer',
+    birth_date: '1961-04-24',
+    first_name: 'Basil',
+    middle_names: 'Middle Name S.'
+  },
+  {
+    id: 10050,
+    emp_no: 10050,
+    gender: 'M',
+    hire_date: '1990-12-25',
+    last_name: 'Dredge',
+    birth_date: '1958-05-21',
+    first_name: 'Yinghua',
+    middle_names: 'Middle Name S.'
+  }
+];
+
+module.exports =  USERS;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var userlist = require('../assets/userlist.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('GET /users : respond with a list of all users');
+  res.type('application/json');
+  res.send(userlist);
 });
+
+// Create a new route that will display the text "You're so cool!" at
+// URL /users/cool/. Test it by running the server and visiting
+// http://localhost:3000/users/cool/ in your browser
+/* GET user's coolness. */
+router.get('/cool', function(req, res, next) {
+  res.send('You\'re so cool!');
+});
+
+/* GET user with userId */
+router.get('/:userId', function(req, res, next) {
+  let userId = req.params.userId;
+  console.log('GET /users/' + userId + ' : respond with data for user with id ' + userId);
+  res.type('application/json');
+  res.send(userlist[userId]);
+});
+
+/* POST ... */
+router.post('/user', function (req, res) {
+  res.send('POST /users/user : got a POST request at /user')
+})
+
+/* PUT ... */
+router.put('/user', function (req, res) {
+  res.send('PUT /users/user : got a PUT request at /user')
+})
+
+/* DELETE ... */
+router.delete('/user/:userID', function (req, res) {
+  let userId = req.params.userId;
+  res.send('DELETE /users/user/' + userId + ' : got a DELETE request at /user for user with id ' +  userId);
+})
+
 
 module.exports = router;
